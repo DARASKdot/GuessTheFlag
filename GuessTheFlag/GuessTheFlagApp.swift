5978e24b3c9f48cb77569e2cb11471f6abeede82
DARASKdot <102633287+DARASKdot@users.noreply.github.com>
2023-06-08 20:25:01 +0900

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Hiroshi Inoue on R 5/06/08.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
