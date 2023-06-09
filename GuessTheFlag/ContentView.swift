dfdf152d5aaf57b3c88f2a4177405741ddaeafc8
DARASKdot <102633287+DARASKdot@users.noreply.github.com>
2023-06-09 21:29:35 +0900

    update app and complete swift100days project2

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -1,22 +1,118 @@
 
 import SwiftUI
 
+import SwiftUI
+
 struct ContentView: View {
     
-    @State private var showingAlert = false
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    
+    @State private var score = 0
+    @State private var attempts = 0
+    @State private var maxAttempts = 10
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+            score += 10
+        } else {
+            scoreTitle = "Wrong"
+        }
+        
+        attempts += 1
+        
+        if attempts == maxAttempts {
+            showingScore = true
+        } else {
+            askQuestion()
+        }
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+    
+    func resetGame() {
+        score = 0
+        attempts = 0
+        askQuestion()
+    }
     
     var body: some View {
-        Button("Show Alert") {
-            showingAlert = true
-        }
-        .alert("Important message", isPresented: $showingAlert) {
-            Button("OK", role: .cancel) { }
-        } message: {
-            Text("Please read this.")
+        
+        ZStack {
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
+            ], center: .top, startRadius: 200, endRadius: 400)
+                .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                
+                Text("Guess the Flag")
+                    .font(.largeTitle.weight(.bold))
+                    .foregroundColor(.white)
+                
+                VStack(spacing: 15) {
+                    VStack {
+                        Text("Tap the flag of")
+                            .font(.subheadline.weight(.heavy))
+                            .foregroundColor(.white)
+                        
+                        Text(countries[correctAnswer])
+                            .font(.largeTitle.weight(.semibold))
+                            .foregroundStyle(.secondary)
+                    }
+                    
+                    ForEach(0..<3) { number in
+                        Button {
+                            flagTapped(number)
+                        } label: {
+                            Image(countries[number])
+                                .renderingMode(.original)
+                                .clipShape(Capsule())
+                                .shadow(radius: 5)
+                        }
+                    }
+                }
+                .padding()
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.regularMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                .alert(isPresented: $showingScore) {
+                    if attempts == maxAttempts {
+                        return Alert(
+                            title: Text("Game Over"),
+                            message: Text("Your final score is \(score)"),
+                            dismissButton: .default(Text("Restart"), action: resetGame)
+                        )
+                    } else {
+                        return Alert(
+                            title: Text(scoreTitle),
+                            message: Text("Your score is \(score)"),
+                            dismissButton: .default(Text("Continue"), action: askQuestion)
+                        )
+                    }
+                }
+                
+                Spacer()
+                
+                Text("Score: \(score)")
+                    .foregroundColor(.white)
+                    .font(.title.bold())
+                
+                Spacer()
+            }
         }
     }
 }
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
